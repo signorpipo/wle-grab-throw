92deb13405174ba78be9f04fb1ac1f81fb75b559
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-05-02 12:16:57 +0200

    Scaling world fix physx not moving

--- wle_grab_throw/js/cauldron/move_target.js
+++ wle_grab_throw/js/cauldron/move_target.js
@@ -1,13 +1,15 @@
 WL.registerComponent('move_target', {
+    _myPhysx: { type: WL.Type.Object }
 }, {
     init: function () {
         PP.EasyTuneVariables.addVariable(new PP.EasyTuneNumber("Target X", 0, 0.5, 4));
-        PP.EasyTuneVariables.addVariable(new PP.EasyTuneNumber("Target Y", 0, 0.5, 4));
-        PP.EasyTuneVariables.addVariable(new PP.EasyTuneNumber("Target Z", -2, 0.5, 4));
+        PP.EasyTuneVariables.addVariable(new PP.EasyTuneNumber("Target Y", 0.25, 0.5, 4));
+        PP.EasyTuneVariables.addVariable(new PP.EasyTuneNumber("Target Z", -3, 0.5, 4));
     },
     start: function () {
     },
     update: function (dt) {
         this.object.setTranslationLocal([PP.EasyTuneVariables.get("Target X").myValue, PP.EasyTuneVariables.get("Target Y").myValue, PP.EasyTuneVariables.get("Target Z").myValue]);
+        this._myPhysx.scalingWorld;
     },
 });
